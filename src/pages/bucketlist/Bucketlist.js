5206a3e0699bc68e62d0d6b19d00c3ccf75c0816
Steven Nooijen <stevennooijen@godatadriven.com>
2020-03-21 19:09:44 +0100

    Add ExploreBar and self-centering map to bucktlist

--- src/pages/bucketlist/Bucketlist.js
+++ src/pages/bucketlist/Bucketlist.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import queryString from 'query-string'
 
 import { withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
@@ -13,13 +14,59 @@ import { fetchSingleDestination } from '../../components/fetching'
 import GetFlickrImages from 'components/destinationCard/GetFlickrImages'
 import FloatingActionButton from './FloatingActionButton'
 
+import ExploreBar from '../explore/components/ExploreBar'
+import { Mapview } from '../../components/mapview'
+import GoogleMap from '../../components/mapview/components/GoogleMap'
+import DestinationPin from '../../components/mapview/components/DestinationPin'
+
 const styles = theme => ({
   loaderContainer: {
     marginTop: theme.spacing(8),
     display: 'flex',
     justifyContent: 'center',
   },
+  body: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100vh',
+  },
+  // in order to position searchHereButton in middle of mapview
+  mapContainer: {
+    flexGrow: 1,
+  },
 })
+
+// Return map bounds based on list of places
+const getMapBounds = (map, maps, places) => {
+  const bounds = new maps.LatLngBounds()
+
+  places.forEach(place => {
+    bounds.extend(
+      // new maps.LatLng(place.geometry.location.lat, place.geometry.location.lng),
+      new maps.LatLng(place.lat, place.lng),
+    )
+  })
+  return bounds
+}
+
+// Re-center map when resizing the window
+const bindResizeListener = (map, maps, bounds) => {
+  maps.event.addDomListenerOnce(map, 'idle', () => {
+    maps.event.addDomListener(window, 'resize', () => {
+      map.fitBounds(bounds)
+    })
+  })
+}
+
+// Fit map to its bounds after the api is loaded
+const apiIsLoaded = (map, maps, places) => {
+  // Get bounds by our places
+  const bounds = getMapBounds(map, maps, places)
+  // Fit map to bounds
+  map.fitBounds(bounds)
+  // Bind the resize listener
+  bindResizeListener(map, maps, bounds)
+}
 
 class Bucketlist extends React.Component {
   constructor(props) {
@@ -27,13 +74,15 @@ class Bucketlist extends React.Component {
 
     this.state = {
       destinationList: [],
-      // likedDestinations: JSON.parse(
-      //   sessionStorage.getItem('likedDestinations'),
-      // ),
+      queryParams: queryString.parse(this.props.location.search),
+      showMap: false,
     }
   }
 
   componentDidMount() {
+    // Set defaults based on query string parameters
+    if (this.state.queryParams.map === 'true') this.setState({ showMap: true })
+
     // Initialize destinations and likes if something in sessionStorage
     const likedDestinationList = JSON.parse(
       sessionStorage.getItem('likedDestinations'),
@@ -90,13 +139,55 @@ class Bucketlist extends React.Component {
     )
   }
 
+  toggleShowMap() {
+    this.setState({ showMap: !this.state.showMap })
+    window.scrollTo(0, 0)
+    // Update query string in url
+    const newQueryParams = this.state.queryParams
+    newQueryParams.map = !this.state.showMap
+    this.pushUrlWithQueryParams(newQueryParams)
+  }
+
+  pushUrlWithQueryParams(queryParams) {
+    this.props.history.push(
+      this.props.location.pathname + '?' + queryString.stringify(queryParams),
+    )
+  }
+
   render() {
     const { classes } = this.props
 
     return (
-      <main>
-        {/* If no likedDestinations, destinationsList is set to null and warning should be displayed */}
-        {this.state.destinationList ? (
+      <div className={classes.body}>
+        <ExploreBar
+          showMap={this.state.showMap}
+          toggleShowMap={() => this.toggleShowMap()}
+        />
+        {this.state.showMap ? (
+          <div className={classes.mapContainer}>
+            <GoogleMap
+              onGoogleApiLoaded={({ map, maps }) =>
+                apiIsLoaded(map, maps, this.state.destinationList)
+              }
+            >
+              {this.state.destinationList.map(place => (
+                <DestinationPin
+                  // this is required to handle clicks
+                  key={place.id}
+                  // these are required by google map to plot
+                  lat={place.lat}
+                  lng={place.lng}
+                  // these are passed along to destinationPin
+                  show={place.id === this.state.showPlace ? true : false}
+                  // place is the object containing all the destination stuff
+                  place={place}
+                  toggleLike={id => this.removeLike(id)}
+                />
+              ))}
+            </GoogleMap>
+          </div>
+        ) : // {/* If no likedDestinations, destinationsList is set to null and warning should be displayed */}
+        this.state.destinationList ? (
           // Show loading indicator till destinations are fetched
           this.state.destinationList.length > 0 ? (
             <div>
@@ -128,7 +219,7 @@ class Bucketlist extends React.Component {
             <WarningCard />
           </Album>
         )}
-      </main>
+      </div>
     )
   }
 }

1ed35bb36f4011178b673108ac7be1b41864e0e9
Steven Nooijen <stevennooijen@godatadriven.com>
2020-05-01 11:26:21 +0200

    Bugfix: disable double click zoom of google maps

--- src/components/mapview/Mapview.js
+++ src/components/mapview/Mapview.js
@@ -24,6 +24,7 @@ const styles = theme => ({
 function createMapOptions(mapGestureHandling) {
   return {
     gestureHandling: mapGestureHandling, // Will capture all touch events on the map towards map panning
+    disableDoubleClickZoom: true,
   }
 }
 

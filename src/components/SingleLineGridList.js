818459ea88eb6bf5e6fe6d4170780adda2ea8c93
Steven Nooijen <stevennooijen@godatadriven.com>
2020-11-03 21:43:15 +0100

    MAJOR make singleLineGridList responsive

--- src/components/SingleLineGridList.js
+++ src/components/SingleLineGridList.js
@@ -1,58 +1,48 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import GridList from '@material-ui/core/GridList'
-import GridListTile from '@material-ui/core/GridListTile'
-import GridListTileBar from '@material-ui/core/GridListTileBar'
-import IconButton from '@material-ui/core/IconButton'
-import StarBorderIcon from '@material-ui/icons/StarBorder'
+import { withStyles } from '@material-ui/core/styles'
+import Grid from '@material-ui/core/Grid'
+
+import DestinationCard from 'components/destinationCard/DestinationCard'
+
 import ExampleList from 'assets/Constants'
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around',
-    overflow: 'hidden',
-    backgroundColor: theme.palette.background.paper,
-  },
+const styles = theme => ({
   gridList: {
+    overflowY: 'auto',
+    listStyle: 'none',
+    WebkitOverflowScrolling: 'touch', // Add iOS momentum scrolling.
     flexWrap: 'nowrap',
     // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
     transform: 'translateZ(0)',
   },
-  title: {
-    color: theme.palette.primary.light,
+  gridItem: {
+    padding: theme.spacing(1),
   },
-  titleBar: {
-    background:
-      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-  },
-}))
+})
 
-export default function SingleLineGridList() {
-  const classes = useStyles()
-
-  return (
-    <div className={classes.root}>
-      <GridList className={classes.gridList} cols={2.2}>
-        {ExampleList.map(tile => (
-          <GridListTile key={tile.id}>
-            <img src={tile.images[0].imgPath} alt={tile.name} />
-            <GridListTileBar
-              title={tile.name}
-              classes={{
-                root: classes.titleBar,
-                title: classes.title,
+class SingleLineGridList extends React.Component {
+  render() {
+    const { classes } = this.props
+    return (
+      <Grid container className={classes.gridList}>
+        {/* Album passes along children components as provided through props */}
+        {ExampleList.map(place => (
+          <Grid item key={place.id} className={classes.gridItem}>
+            <DestinationCard
+              style={{ minWidth: 200 }}
+              className={classes.tile}
+              place={place}
+              // toggleLike={id => this.toggleLike(id)}
+              onClick={() => {
+                // send to destination page
+                this.props.history.push('/explore/' + place.id)
               }}
-              actionIcon={
-                <IconButton aria-label={`star ${tile.name}`}>
-                  <StarBorderIcon className={classes.title} />
-                </IconButton>
-              }
             />
-          </GridListTile>
+          </Grid>
         ))}
-      </GridList>
-    </div>
-  )
+      </Grid>
+    )
+  }
 }
+
+export default withStyles(styles)(SingleLineGridList)
